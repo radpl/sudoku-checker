3c5d7420f7986c8c28548ccbc38d1172f0b17435
Radoslaw Plonka <radoslaw.a.plonka@gmail.com>
2019-11-14 23:35:20 +0100

    Calculation logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
 import './App.css';
 import SudokuForm from './SudokuForm';
+import ResultTable from './ResultTable';
 
 export default class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      solvedSudoku: ""
+      initialSudoku: "",
+      arraySudoku: [],
+      valuesRows: [],
+      valuesCols: [],
+      valuesSqrs: [],
+      status: {
+        rows: false,
+        cols: false,
+        sqrs: false
+      }
     }
 
     this.handleChange = this.handleChange.bind(this);
@@ -18,18 +28,59 @@ export default class App extends Component {
     const { value } = event.target;
     this.setState({
       initialSudoku: value,
-      solvedSudoku: []
     });
   }
 
   handleSubmit(event) {
     event.preventDefault();
+    const sudoku = this.state.initialSudoku;
+    const arrPre = JSON.parse(sudoku);
+    const midArr = arrPre.reduce((acc, item) => acc.concat([item.split(",")]), []);
+    const arrPost = arrPre.reduce((acc, item) => acc.concat(item.split(",").map(Number)), []);
+
+    this.setState({
+      arraySudoku: [...midArr]
+    });
+
+    this.calculate(arrPost);
+
   }
 
-  calculate() {
+  calculate(arrPost) {
+    //Calculate
+    let rows = [];
+    let cols = [];
+    let sqrs = new Array(9).fill(0);
+    for (let i = 0; i < 9; i++) {
+      let sumRow = 0;
+      let sumCol = 0
+      for (let j = 0; j < 9; j++) {
+        sumRow += arrPost[i * 9 + j];
+        sumCol += arrPost[j * 9 + i];
+        var t1 = Math.floor(j / 3);
+        var t2 = Math.floor(i / 3);
+        sqrs[t1 + t2 * 3] += arrPost[i * 9 + j];
+      }
+      rows.push(sumRow);
+      cols.push(sumCol);
+    }
+
+    this.setState({
+      valuesRows: [...rows],
+      valuesCols: [...cols],
+      valuesSqrs: [...sqrs],
+      status: {
+        rows: this.checkStatus(rows),
+        cols: this.checkStatus(cols),
+        sqrs: this.checkStatus(sqrs)
+      }
+    });
 
   }
 
+  checkStatus(data) {
+    return data.every(element => element === 45);
+  }
   render() {
     return (
       <div className="container">
@@ -42,8 +93,13 @@ export default class App extends Component {
           />
         </div>
         <div>
-          <h2>Calculation result</h2>
-          <ResultTable data={this.state.solvedSudoku} />
+          <h2>Loaded sudoku</h2>
+          <ResultTable
+            data={this.state.arraySudoku}
+            rows={this.state.valuesRows}
+            cols={this.state.valuesCols}
+            sqrs={this.state.valuesSqrs}
+            status={this.state.status} />
         </div>
       </div>
     );
--- src/ResultTable.js
+++ src/ResultTable.js
@@ -1,12 +1,25 @@
 import React from 'react'
 
-function ResultTable({ data }) {
+function ResultTable({ data, rows, cols, sqrs, status }) {
+
+
     return (
-        <div>
+        <div className="container">
             <table>
+                {data.map((item) => {
+                    return (<tr>
+                        {item.map(internalItem => <td>{internalItem}</td>)}
+                    </tr>);
+                })}
             </table>
+            <div className="result">Rows result {status.rows ? " OK" : " NOT OK"}</div>
+            {rows.map((item, index) => " row " + index + ":" + item)}
+            <div className="result">Columns result {status.cols ? " OK" : " NOT OK"}</div>
+            {cols.map((item, index) => " col " + index + ":" + item)}
+            <div className="result">Squares result {status.sqrs ? " OK" : " NOT OK"}</div>
+            {sqrs.map((item, index) => " sqr " + index + ":" + item)}
         </div>
-    )
+    );
 }
 
 export default ResultTable
